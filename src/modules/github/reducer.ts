628cc9f0a0fee9acb684756c21b9b028438655c6
BigbrotherShin <shinjhhp5@gmail.com>
2020-01-12 16:26:54 +0900

    reducer typescript refactoring 4

--- src/modules/github/reducer.ts
+++ src/modules/github/reducer.ts
@@ -1,29 +1,21 @@
 import { GithubState, GithubAction } from './types';
 import { createReducer } from 'typesafe-actions';
-import {
-  GET_USER_PROFILE_ERROR,
-  GET_USER_PROFILE,
-  GET_USER_PROFILE_SUCCESS,
-} from './actions';
-import { asyncState } from '../../lib/reducerUtils';
+import { getUserProfileAsync } from './actions';
+import { asyncState, createAsyncReducer } from '../../lib/reducerUtils';
 
 const initialState: GithubState = {
   userProfile: asyncState.initial(),
 };
 
-const github = createReducer<GithubState, GithubAction>(initialState, {
-  [GET_USER_PROFILE]: state => ({
-    ...state,
-    userProfile: asyncState.load(),
-  }),
-  [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
-    ...state,
-    userProfile: asyncState.success(action.payload),
-  }),
-  [GET_USER_PROFILE_ERROR]: (state, action) => ({
-    ...state,
-    userProfile: asyncState.error(action.payload),
-  }),
-});
+const github = createReducer<GithubState, GithubAction>(
+  initialState,
+).handleAction(
+  [
+    getUserProfileAsync.request,
+    getUserProfileAsync.success,
+    getUserProfileAsync.failure,
+  ],
+  createAsyncReducer(getUserProfileAsync, 'userProfile'),
+);
 
 export default github;
